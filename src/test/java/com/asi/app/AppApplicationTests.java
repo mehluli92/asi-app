c9c8e92b30f4c72af82f3300b67bdbdc45f12e56
mehluli92 <nokwaramehluli@gmail.com>
2023-04-13 18:35:41 +0200

    ASI APP

---
+++ src/test/java/com/asi/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
